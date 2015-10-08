51da0e27c6567b0647d98846974b05995d1be435
dogdog7788 <dogdog7788@qq.com>
2015-10-08 12:04:01 +0800

    最新数据结构提交并清理测试数据

--- sql/safe_check.sql
+++ sql/safe_check.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2015-09-21 21:51:07
+Date: 2015-10-08 12:03:09
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,10 +22,10 @@ DROP TABLE IF EXISTS `admin_user`;
 CREATE TABLE `admin_user` (
   `user_name` varchar(32) NOT NULL,
   `pass_word` varchar(32) NOT NULL,
-  `name` varchar(16) NOT NULL,
-  `sex` int(11) NOT NULL,
-  `birthday` date NOT NULL,
-  `call` varchar(32) NOT NULL,
+  `name` varchar(16) DEFAULT '',
+  `sex` int(11) DEFAULT '1',
+  `birthday` date DEFAULT NULL,
+  `call` varchar(32) DEFAULT '',
   `remark` varchar(512) DEFAULT NULL,
   `up_user` varchar(32) DEFAULT NULL,
   `status` int(11) NOT NULL DEFAULT '0',
@@ -35,10 +35,7 @@ CREATE TABLE `admin_user` (
 -- ----------------------------
 -- Records of admin_user
 -- ----------------------------
-INSERT INTO `admin_user` VALUES ('aa', 'LpkBu6U7/DzcaD1DryBisxxpVBg=', 'aa', '1', '2015-09-16', 'aa', 'aa', 'xx', '0');
-INSERT INTO `admin_user` VALUES ('xx', 'LpkBu6U7/DzcaD1DryBisxxpVBg=', 'xx', '1', '2015-09-11', '111', '1', '', '0');
-INSERT INTO `admin_user` VALUES ('yy', 'yy', 'yy', '1', '2015-09-16', '11', '1', 'xx', '1');
-INSERT INTO `admin_user` VALUES ('zz', 'zz', 'zz', '1', '2015-09-16', 'zz', 'zz', 'xx', '1');
+INSERT INTO `admin_user` VALUES ('admin', 'FoxpBwf+x4bDmWQtNCx7vojz8Oc=', '超级管理员', '1', '2015-09-11', '111', '超级管理员', '', '0');
 
 -- ----------------------------
 -- Table structure for check_items
@@ -50,13 +47,11 @@ CREATE TABLE `check_items` (
   `item_content` text NOT NULL,
   `add_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of check_items
 -- ----------------------------
-INSERT INTO `check_items` VALUES ('1', 'adfasdfas', 'asdfasdf我是这个世界的主人', '2015-09-18 20:33:17');
-INSERT INTO `check_items` VALUES ('2', '2', '2', '2015-09-18 20:33:24');
 
 -- ----------------------------
 -- Table structure for check_record
@@ -74,23 +69,11 @@ CREATE TABLE `check_record` (
   `check_server_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `cp_Name` (`cp_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of check_record
 -- ----------------------------
-INSERT INTO `check_record` VALUES ('1', '俗人工作室', '1,2', 'xxx', '1', '4', '112.914604,28.232497', '2015-09-11 12:53:40', '2015-09-11 12:56:00');
-INSERT INTO `check_record` VALUES ('2', 'cp2', 'xxx', 'aa_down', '刘德华', '4', '112.914604,28.232497', '2015-09-11 11:11:45', '2015-09-17 11:11:49');
-INSERT INTO `check_record` VALUES ('3', 'cp2', 'xxx', 'aa_down', '刘', '4', '112.914604,28.232497', '2015-09-08 11:12:19', '2015-09-08 11:12:19');
-INSERT INTO `check_record` VALUES ('4', 'cp2', 'xxx', 'aa_down', '刘', '4', '112.914604,28.232497', '2015-09-07 11:12:19', '2015-09-07 11:12:19');
-INSERT INTO `check_record` VALUES ('5', 'cp2', 'xxx', 'aa_down', '刘', '4', '112.914604,28.232497', '2015-09-06 11:12:19', '2015-09-06 11:12:19');
-INSERT INTO `check_record` VALUES ('6', 'cp2', 'xxx', 'aa_down', '刘', '4', '112.914604,28.232497', '2015-09-05 11:12:19', '2015-09-05 11:12:19');
-INSERT INTO `check_record` VALUES ('7', 'cp2', 'xxx', 'aa_down', '刘', '4', '112.914604,28.232497', '2015-09-04 11:12:19', '2015-09-04 11:12:19');
-INSERT INTO `check_record` VALUES ('8', 'cp2', 'xxx', 'aa_down', '刘', '4', '112.914604,28.232497', '2015-09-03 11:12:19', '2015-09-03 11:12:19');
-INSERT INTO `check_record` VALUES ('9', 'cp2', 'xxx', 'aa_down', '刘', '4', '112.914604,28.232497', '2015-09-02 11:12:19', '2015-09-02 11:12:19');
-INSERT INTO `check_record` VALUES ('10', 'cp2', 'xxx', 'aa_down', '刘', '4', '112.914604,28.232497', '2015-09-01 11:12:19', '2015-09-01 11:12:19');
-INSERT INTO `check_record` VALUES ('11', 'cp2', 'xxx', 'aa_down', '刘', '4', '112.914604,28.232497', '2015-08-08 11:12:19', '2015-08-08 11:12:19');
-INSERT INTO `check_record` VALUES ('12', 'cp2', 'xxx', 'aa_down', '刘', '4', '112.914604,28.232497', '2015-08-07 11:12:19', '2015-08-07 11:12:19');
 
 -- ----------------------------
 -- Table structure for enforce_record
@@ -106,15 +89,11 @@ CREATE TABLE `enforce_record` (
   `enforce_server_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `cp_name` (`cp_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of enforce_record
 -- ----------------------------
-INSERT INTO `enforce_record` VALUES ('1', '俗人工作室', 'xx', 'xx_name', '5', '2015-09-11 12:53:41', '2015-09-11 12:57:41');
-INSERT INTO `enforce_record` VALUES ('2', '俗人工作室', 'yy', 'yy_name', '5', '2015-09-12 11:50:45', '2015-09-12 11:50:51');
-INSERT INTO `enforce_record` VALUES ('3', '几把工作室', 'zz', 'zz_name', '5', '2015-09-01 11:16:30', '2015-09-16 11:16:34');
-INSERT INTO `enforce_record` VALUES ('4', '几把二工作室', 'aa_down', 'zz_name', '5', '2015-10-01 11:16:55', '2015-08-01 11:16:57');
 
 -- ----------------------------
 -- Table structure for notice
@@ -128,14 +107,11 @@ CREATE TABLE `notice` (
   `add_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `status,add_time` (`status`,`add_time`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of notice
 -- ----------------------------
-INSERT INTO `notice` VALUES ('9', 'asdf111', 'asdf', '1', '2015-09-18 21:10:34');
-INSERT INTO `notice` VALUES ('10', 'ttt', 'ttt', '0', '2015-09-19 16:07:46');
-INSERT INTO `notice` VALUES ('11', 'tt2', 'tt2', '2', '2015-09-19 16:07:57');
 
 -- ----------------------------
 -- Table structure for resource
@@ -149,16 +125,11 @@ CREATE TABLE `resource` (
   `updated_time` datetime DEFAULT NULL,
   `created_time` datetime DEFAULT NULL,
   PRIMARY KEY (`resource_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of resource
 -- ----------------------------
-INSERT INTO `resource` VALUES ('1', '20b545561a9fb741.jpg', '20b545561a9fb741.jpg', '20b545561a9fb741.jpg', null, '2015-09-10 13:26:45');
-INSERT INTO `resource` VALUES ('2', '20b545561a9fb741.jpg', '20b545561a9fb741.jpg', '20b545561a9fb741.jpg', null, '2015-09-10 13:28:23');
-INSERT INTO `resource` VALUES ('3', '20b545561a9fb741.jpg', '20b545561a9fb741.jpg', '20b545561a9fb741.jpg', '2015-09-10 13:29:10', '2015-09-10 13:29:10');
-INSERT INTO `resource` VALUES ('4', '20b545561a9fb741.jpg', '20b545561a9fb741.jpg', '20b545561a9fb741.jpg', '2015-09-11 14:04:12', '2015-09-11 12:56:00');
-INSERT INTO `resource` VALUES ('5', '20b545561a9fb741.jpg', '20b545561a9fb741.jpg', '20b545561a9fb741.jpg', '2015-09-11 12:57:41', '2015-09-11 12:57:41');
 
 -- ----------------------------
 -- Table structure for user
@@ -167,10 +138,10 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_name` varchar(32) NOT NULL,
   `pass_word` varchar(32) NOT NULL,
-  `name` varchar(16) NOT NULL,
-  `sex` int(11) NOT NULL,
-  `birthday` date NOT NULL,
-  `call` varchar(32) NOT NULL,
+  `name` varchar(16) DEFAULT NULL,
+  `sex` int(11) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `call` varchar(32) DEFAULT NULL,
   `remark` varchar(512) DEFAULT NULL,
   `up_user` varchar(32) DEFAULT NULL,
   `status` int(11) NOT NULL DEFAULT '0',
@@ -180,6 +151,4 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('aa_down', 'aa', 'aa_name', '1', '2015-09-16', 'aa_down', 'aa_down', 'aa', '0');
-INSERT INTO `user` VALUES ('xx_down', 'LpkBu6U7/DzcaD1DryBisxxpVBg=', 'xx_name', '1', '2015-09-10', '111', '11', 'xx', '0');
-INSERT INTO `user` VALUES ('yy_down', 'yy', 'yy_name', '1', '2015-09-16', 'yy_down', 'yy_down', 'yy', '0');
+INSERT INTO `user` VALUES ('xx_down', 'LpkBu6U7/DzcaD1DryBisxxpVBg=', '测试账号', '1', '2015-10-07', '', '测试账号', 'admin', '0');
